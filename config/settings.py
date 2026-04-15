a4ee27cb614e92392bb29e35bb7c40cf7356c3a8
Koffi-Cobbin <ocupualorelijah@gmail.com>
2026-04-15 10:10:36 +0000

    updated CORS origins

--- config/settings.py
+++ config/settings.py
@@ -219,6 +219,8 @@ else:
         'https://kofficobbin.pythonanywhere.com',
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://localhost:5000",
+        "http://127.0.0.1:5000"
     ]
 
 CORS_ALLOW_CREDENTIALS = True
