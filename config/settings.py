0c7e03099a805c2144cde5bb6650236991ee133c
Koffi-Cobbin <ocupualorelijah@gmail.com>
2026-04-26 19:41:44 +0000

    smpt bug fix

--- config/settings.py
+++ config/settings.py
@@ -219,7 +219,7 @@ AUTH_USER_MODEL = 'musewave.User'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
